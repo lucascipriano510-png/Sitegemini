3710f2b92b4678857a07263c528104cbd65167eb
lucascipriano510-png <lucascipriano510@gmail.com>
2026-04-20 14:18:28 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import  App  from './App';
+import App from './App';
+import './style.css'; 
 
 const root = createRoot(document.getElementById('app'));
 
 root.render(
   <StrictMode>
-    <App name="StackBlitz" />
+    <App />
   </StrictMode>
 );
